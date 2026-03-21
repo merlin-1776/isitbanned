f499d02c43be3de7a22afec9008b347f9a6e1815
Ross Rojek <rossrojek@Rosss-Mac-mini.attlocal.net>
2026-03-21 08:25:56 -0700

    Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://merlin-1776.github.io',
+	base: '/isitbanned',
 	integrations: [mdx(), sitemap()],
 });
